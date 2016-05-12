70483dc20e4f7e09f3634cc83bf4ff89d7ffa399
Suvigya Tripathi <suvigya91@gmail.com>
2016-05-12 14:56:48 -0400

    added input files

---
+++ inp/test.py
@@ -0,0 +1,24 @@
+import json
+import os
+import re
+import subprocess
+import time
+import sys
+import argparse
+import imp
+import glob
+import shutil
+import glob
+from colorama import *
+
+
+if __name__ == "__main__":
+	print(Fore.GREEN + 'Green text'+Fore.RESET)
+	#print(Fore.RED + 'hello')
+	#print(Fore.RESET)
+	#print glob.glob('*')
+	#p = subprocess.Popen(['scp','md_1_1.ncdf' , 'tg831932@stampede.tacc.utexas.edu:${HOME}/Standalone/user_files'])
+                                 #stdin=subprocess.PIPE,
+                                 #stdout=subprocess.PIPE,
+                                 #stderr=subprocess.PIPE)
+	#stdout, stderr = p.communicate()
